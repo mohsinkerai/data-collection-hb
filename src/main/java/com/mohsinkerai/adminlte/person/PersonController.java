bfcae291a9c36beb195999f54508de5ef1f4479e
Mohsin Kerai <mohsin.kerai@gmail.com>
2019-10-06 19:05:59 +0500

    Validations FTW

--- src/main/java/com/mohsinkerai/adminlte/person/PersonController.java
+++ src/main/java/com/mohsinkerai/adminlte/person/PersonController.java
@@ -9,7 +9,10 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping(PersonController.URL_PATH)
@@ -53,11 +56,21 @@ public class PersonController extends SimpleBaseController<Person> {
   }
 
   @Override
-  public String edit(Long id, Model model) {
+  public String edit(@PathVariable Long id, Model model) {
     boolean canEditPerson = personService.isPersonEditAllowed(id);
-    if(!canEditPerson) {
+    if (!canEditPerson) {
       throw new RuntimeException("Person has Exceeded 2 Days and Cannot be Edited");
     }
     return super.edit(id, model);
+  }
+
+  @Override
+  public String save(Person person, BindingResult bindingResult, Model model,
+    RedirectAttributes ra) {
+    super.save(person, bindingResult, model, ra);
+    ra.addFlashAttribute("successFlash", String
+      .format("Successfully Saved Jamati Memeber with Name <b>%s</b> and id </b>%d</b>", person.getName(),
+        person.getId()));
+    return "redirect:/" + urlPath() + "/add";
   }
 }
